a214f86bb1a02b85915eca8a64c38987a1d67183
Pichardo <angelpichardorivera1@gmail.com>
2026-05-24 01:24:52 -0600

    primerComit

---
+++ src/main/java/com/mycompany/ringcard/RingCard.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ringcard;
+
+/**
+ *
+ * @author DELL
+ */
+public class RingCard {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
